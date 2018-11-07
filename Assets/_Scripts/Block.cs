13a2d6a803d1d24122d0b5485ef21755a33ce93a
Sourav Chatterjee <sromic@gmail.com>
2018-11-07 15:24:59 +0530

    [Update] Basic Gameplay completed with Recursive flooding for unmined blocks.

---
+++ Assets/_Scripts/Block.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minesweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Block : MonoBehaviour
+    {
+        private SpriteRenderer BlockSpriteRenderer;
+
+        private int index;
+        public int Index
+        {
+            get { return index; }
+            set
+            {
+                index = value;
+            }
+        }
+        public List<Block> NeighbourBlocks;
+        [SerializeField]private BlockMineStatus mineStatus;
+        public BlockMineStatus MineStatus
+        {
+            get { return mineStatus; }
+            set
+            {
+                mineStatus = value;
+                //if(mineStatus != BlockMineStatus.Mined)
+                //{
+                //    MineText.text = "0";
+                //}
+                //else
+                //{
+                //    MineText.text = "1";
+                //}
+            }
+        }
+        private BlockFlagStatus FlagStatus;
+
+        public Text MineText;
+
+        private BlockStatus blockStatus;
+        public BlockStatus BlockStatus
+        {
+            get { return blockStatus; }
+            set
+            {
+                blockStatus = value;
+                if(blockStatus == BlockStatus.Opened)
+                {
+                    if (mineStatus == BlockMineStatus.Mined)
+                    {
+                        BlockSpriteRenderer.sprite = GameManager.Instance.MinedBlockSprite;
+                    }
+                    else
+                    {
+                        BlockSpriteRenderer.sprite = GameManager.Instance.OpenedBlockSprite;
+                    }
+                }
+                else if(blockStatus == BlockStatus.UnOpened)
+                {
+                    if (FlagStatus == BlockFlagStatus.Flagged)
+                    {
+                        BlockSpriteRenderer.sprite = GameManager.Instance.FlaggedBlockSprite;
+                    }
+                    else
+                    {
+                        BlockSpriteRenderer.sprite = GameManager.Instance.UnopenedBlockSprite;
+                    }
+                }
+            }
+        }
+
+        private int numberOfNeighbouringMines;
+        public int NumberOfNeighbouringMines
+        {
+            get { return numberOfNeighbouringMines; }
+            set
+            {
+                numberOfNeighbouringMines = value;
+                MineText.text = numberOfNeighbouringMines.ToString();
+            }
+        }
+
+        private void Awake()
+        {
+            BlockSpriteRenderer = GetComponent<SpriteRenderer>();
+            InitializeBlock();
+        }
+
+        public void InitializeBlock(int index = 0)
+        {
+            Index = index;
+            MineText.text = "";
+            NeighbourBlocks = new List<Block>();
+            MineStatus = BlockMineStatus.UnMined;
+            FlagStatus = BlockFlagStatus.UnFlagged;
+            BlockStatus = BlockStatus.UnOpened;
+            NumberOfNeighbouringMines = 0;
+            HideMineText();
+            
+        }
+
+        public void AddNeighbouringBlocks(Block[]Blocks)
+        {
+            int BoardValue = GameManager.Instance.BoardValue;
+
+            if(index + BoardValue >= 0 && index + BoardValue < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index + BoardValue]);
+
+            if (index - BoardValue >= 0 && index - BoardValue < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index - BoardValue]);
+
+            if (index + 1 >= 0 && index + 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index + 1]);
+
+            if (index - 1 >= 0 && index - 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index - 1]);
+
+            if (index + BoardValue + 1 >= 0 && index + BoardValue + 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index + BoardValue + 1]);
+
+            if (index + BoardValue - 1 >= 0 && index + BoardValue - 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index + BoardValue - 1]);
+
+            if (index - BoardValue + 1 >= 0 && index - BoardValue + 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index - BoardValue + 1]);
+
+            if (index - BoardValue - 1 >= 0 && index - BoardValue - 1 < BoardValue * BoardValue)
+                NeighbourBlocks.Add(Blocks[index - BoardValue - 1]);
+
+            CalculateNeighbouringMines();
+
+        }
+
+        private void CalculateNeighbouringMines()
+        {
+            for (int i = 0; i < NeighbourBlocks.Count; i++)
+            {
+                if (NeighbourBlocks[i].MineStatus == BlockMineStatus.Mined)
+                    NumberOfNeighbouringMines++;
+            }
+        }
+
+        public void MineBlock()
+        {
+            MineStatus = BlockMineStatus.Mined;
+        }
+
+        //TESTING
+        public void UncoverBlock()
+        {
+            //BlockSpriteRenderer.color = Color.red;
+            BlockStatus = BlockStatus.Opened;
+            //HideMineText();
+        }
+
+        public void HideMineText()
+        {
+            MineText.gameObject.SetActive(false);
+        }
+        public void ShowMineText()
+        {
+            MineText.gameObject.SetActive(true);
+        }
+
+        private void OnMouseDown()
+        {
+            //UncoverBlock();
+            GameManager.Instance.BlockClicked(this);
+        }
+    }
+
+    public enum BlockMineStatus
+    {
+        UnMined,
+        Mined
+    }
+
+    public enum BlockStatus
+    {
+        UnOpened,
+        Opened
+    }
+
+    public enum BlockFlagStatus
+    {
+        UnFlagged,
+        Flagged
+    }
+}
---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        public Sprite UnopenedBlockSprite;
+        public Sprite OpenedBlockSprite;
+        public Sprite FlaggedBlockSprite;
+        public Sprite MinedBlockSprite;
+        public GameObject BlockPrefab;
+        public Transform BlockHolder;
+        public int BoardValue;
+        public int NumberOfMines;
+
+        public Camera Camera;
+
+        public Vector3 shiftAfterGeneration;
+        public Vector2 blockExtent;
+
+        public float xShift;
+        public float yShift;
+
+        public Block[] blocks;
+
+        private List<Block> BlocksVisited;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            blocks = new Block[BoardValue * BoardValue];
+            BlocksVisited = new List<Block>();
+            GenerateBoard();
+            BlockHolder.transform.position = new Vector3(shiftAfterGeneration.x, shiftAfterGeneration.y, BlockHolder.transform.position.z);
+            MineBlocks();
+            SetNeighbouringMines();
+        }
+
+        //Generate board
+        private void GenerateBoard()
+        {
+            Vector2 pos = Vector2.zero;
+            int index = 0;
+            for (int i = 0; i < BoardValue; i++)
+            {
+                pos.y = 0;
+                for (int j = 0; j < BoardValue; j++)
+                {
+                    GameObject block = Instantiate(BlockPrefab, BlockHolder);
+                    //logical grid
+                    blocks[index] = block.GetComponent<Block>();
+                    blocks[index].Index = index;
+                    index++;
+                    //physical grid
+                    block.transform.position = pos;
+                    pos.y = pos.y + yShift;
+                }
+                pos.x = pos.x + xShift;
+            }
+        }
+
+        private void MineBlocks()
+        {
+            List<int> indexOfMines = new List<int>();
+            for (int i = 0; i < NumberOfMines; i++)
+            {
+                int mineIndex = Random.Range(0, BoardValue * BoardValue);
+                while(indexOfMines.Contains(mineIndex))
+                {
+                    mineIndex = Random.Range(0, BoardValue * BoardValue);
+                }
+                indexOfMines.Add(mineIndex);
+                blocks[mineIndex].MineBlock();
+            }
+        }
+
+        private void SetNeighbouringMines()
+        {
+            for (int i = 0; i < BoardValue * BoardValue; i++)
+            {
+                blocks[i].AddNeighbouringBlocks(blocks);
+            }
+        }
+
+        //public void UncoverBlock()
+        //{
+        //    for (int i = 0; i < BoardValue * BoardValue; i++)
+        //    {
+        //        blocks[i].UncoverBlock();
+        //    }
+        //}
+
+        public void BlockClicked(Block b)
+        {
+            if (b.MineStatus == BlockMineStatus.Mined)
+            {
+
+            }
+                //GameOver;
+            else
+            {
+                UncoverBlock(b);
+                BlockOpen();
+            }
+        }
+
+        private void UncoverBlock(Block b)
+        {
+            //b.UncoverBlock();
+            //BlocksToUncover.Add(b);
+            if(b.NumberOfNeighbouringMines > 0)
+            {
+                BlocksVisited.Add(b);
+                return;
+            }
+
+            if (BlocksVisited.Contains(b))
+            {
+                //b.ShowMineText();
+                return;
+            }
+            else
+            {
+                BlocksVisited.Add(b);
+
+                for (int i = 0; i < b.NeighbourBlocks.Count; i++)
+                {
+                    UncoverBlock(b.NeighbourBlocks[i]);
+                }
+            }
+        }
+
+        private void BlockOpen()
+        {
+            for (int i = 0; i < BlocksVisited.Count; i++)
+            {
+                BlocksVisited[i].UncoverBlock();
+                if(BlocksVisited[i].NumberOfNeighbouringMines > 0)
+                {
+                    BlocksVisited[i].ShowMineText();
+                }
+            }
+        }
+
+        //Listen for input
+        //Open tiles
+        //Determine if game is over or won
+        //etc.
+    }
+}
